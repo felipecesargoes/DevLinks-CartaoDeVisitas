65bb20afc38406a745061ccc6122e7e0b8ad688b
Felipe Góes <felipecesargoes@gmail.com>
2023-11-11 15:14:52 -0300

    :tada: initial commit

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,26 +1,18 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  
+
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
   //substituir a imagem
-  if(html.classList.contains("light")) {
+  if (html.classList.contains("light")) {
     //se tiver light mode, adicionar imagem light
     img.setAttribute("src", "./assets/eubp1.png")
-    img.setAttribute("alt","Foto em branco e preto de Felipe Góes sorrindo")
+    img.setAttribute("alt", "Foto em branco e preto de Felipe Góes sorrindo")
   } else {
     //se tiver sem light mode, manter imagem normal
     img.setAttribute("src", "./assets/eu1.png")
-    img.setAttribute("alt","Foto colorida de Felipe Góes sorrindo") 
-
+    img.setAttribute("alt", "Foto colorida de Felipe Góes sorrindo")
   }
-  
-
-  
-
-  
-  
-  
 }
